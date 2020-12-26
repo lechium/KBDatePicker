8217b7630cd1ab52e8e3393f5a4e5b8febbb0e98
Kevin Bradley <kevin.w.bradley@me.com>
2020-12-26 10:49:08 -0700

    oops

---
+++ KBDatePicker/KBDatePickerViewController.h
@@ -0,0 +1,16 @@
+//
+//  KBDatePickerViewController.h
+//  KBDatePicker
+//
+//  Created by Kevin Bradley on 12/26/20.
+//  Copyright © 2020 nito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBDatePickerViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property NSDate *currentDate;
+
+@end
+
