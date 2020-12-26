fc85840337ab08fd9284e304998077229c786bbc
Kevin Bradley <kevin.w.bradley@me.com>
2020-12-26 10:42:31 -0700

    initial commit

---
+++ KBDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KBDatePicker
+//
+//  Created by Kevin Bradley on 12/26/20.
+//  Copyright © 2020 nito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KBDatePicker/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  KBDatePicker
+//
+//  Created by Kevin Bradley on 12/26/20.
+//  Copyright © 2020 nito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
